1c11b9512d27997b0184a8fcf7e24602357ced0f
Hahyung Jung <master@Hahyungs-MacBook-Pro.local>
2022-02-27 16:08:12 -0500

    update

---
+++ Investigating Texts and Calls/project 0/Task1.py
@@ -0,0 +1,42 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files.
+"""
+import csv
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+
+"""
+TASK 1:
+How many different telephone numbers are there in the records? 
+Print a message:
+"There are <count> different telephone numbers in the records."
+"""
+
+def task1(texts, calls):
+
+    numbers = []
+
+    for i in range(len(texts)):
+        numbers.append(texts[i][0])
+        numbers.append(texts[i][1])
+
+    for i in range(len(calls)):
+        numbers.append(calls[i][0])
+        numbers.append(calls[i][1])
+
+    result = []
+
+    for value in numbers:
+        if value not in result:
+            result.append(value)
+
+    print("There are", len(result) ,"different telephone numbers in the records.")
+
+    return(result)
---
+++ Investigating Texts and Calls/project 0/Task4.py
@@ -0,0 +1,54 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files.
+"""
+import csv
+
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+"""
+TASK 4:
+The telephone company want to identify numbers that might be doing
+telephone marketing. Create a set of possible telemarketers:
+these are numbers that make outgoing calls but never send texts,
+receive texts or receive incoming calls.
+
+Print a message:
+"These numbers could be telemarketers: "
+<list of numbers>
+The list of numbers should be print out one per line in lexicographic order with no duplicates.
+"""
+
+def task4(texts, calls):
+
+    callers = set()
+    called = set()
+    texters = set()
+    texted = set()
+
+    for i in range(len(calls)):
+       callers.add(calls[i][0])
+       called.add(calls[i][1])
+
+    for i in range(len(texts)):
+        texters.add(texts[i][0])
+        texted.add(texts[i][1])
+
+    callers = callers.difference(called)
+    callers = callers.difference(texters)
+    callers = callers.difference(texted)
+    print(len(callers))
+
+    print("These numbers could be telemarketers: ", sorted(callers))
+
+
+
+
+
+
